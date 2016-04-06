840610f77045892fd3ea47c404953c00548369f5
Bhagyashri Pachkor <bhagyashrip547@gmail.com>
2016-04-06 10:15:49 +0530

    Create vowelconsonant.c

---
+++ vowelconsonant.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+
+  char c;
+  printf("Enter an alphabet: ");
+  scanf("%c",&c);
+  if(c=='a'||c=='A'||c=='e'||c=='E'||c=='i'||c=='I'||c=='o'||c=='O'||c=='u'||c=='U')
+       printf("%c is a vowel.",c);
+  else
+       printf("%c is a consonant.",c);
+  
+}
