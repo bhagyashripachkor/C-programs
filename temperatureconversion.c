2efbfcaaa1fa86913fa7a61d2563ce627518b5d9
Bhagyashri Pachkor <bhagyashrip547@gmail.com>
2016-04-06 10:15:21 +0530

    Create temperatureconversion.c

---
+++ temperatureconversion.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+void main()
+{
+	/*float centigrade=0.0;
+	float fahrenheit=0.0;
+	printf("conversion from Centigrade to Fahrenheit");
+	printf("enter the temperature in Centigrade");
+	scanf("%f",&centigrade);
+	fahrenheit=(1.8*centigrade) +32;
+	printf("Temperature in Fahrenheit is %f",fahrenheit);
+	
+	printf("conversion from Fahrenheit to Centigrade");
+	printf("enter the temperature in Fahrenheit");
+	scanf("%f",&fahrenheit);
+	
+	centigrade=(fahrenheit-32) * 5/9;
+	printf("Temperature in Centigrade is %f",centigrade);*/
+
+	/*float rupees=0.0;
+	float dollar=0.0;
+	printf("Enter the amount in rupees;\n");
+	scanf("%f",&rupees);
+	dollar=rupees/63.6600;;
+	printf("dollars=%0.06f\n",dollar);
+	printf("Enter the amount in dollars;\n");
+	scanf("%f",&dollar);
+	rupees=dollar*63.6600; 
+	printf("rupees=%0.06f\n",rupees);*/
+
+	/*float miles=0.0;
+	float km=0.0;
+	printf("enter the distance in km");
+	scanf("%f",&km);
+	miles=km/1.609344;
+	printf("miles =%.06f",miles);
+	printf("enter the distance in miles");
+	scanf("%f",&miles);
+	km=miles * 1.609344;
+	printf("kilometer=%.06f",km);*/
+	
+
+
+	/*float kg=0.0;
+	float pounds=0.0;
+	printf("enter the value in kilograms");
+	scanf("%f",&kg);
+	pounds=kg*2.204624;
+	printf("pounds=%.06f",pounds);
+	printf("enter the value in pounds");
+	scanf("%f",&pounds);
+	kg=pounds/2.204624;
+	printf("kilograms=%.06f",kg);*/
+	
+	float litres=0.0;
+	float gallons=0.0;
+	printf("enter the value in litres");
+	scanf("%f",&litres);
+	gallons=litres*0.264172;
+	printf("gallons=%.06f",gallons);
+	printf("enter the value in gallons");
+	scanf("%f",&gallons);
+	litres=gallons/0.264172;
+	printf("litres=%.06f",litres);
+
+}
