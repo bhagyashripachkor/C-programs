d478ec674fcc9300f2e98ae058338d45b55f0e83
Bhagyashri Pachkor <bhagyashrip547@gmail.com>
2016-04-07 21:16:49 +0530

    Create case.c

---
+++ case.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+	char input;
+	scanf("%c",&input);
+	switch(input)
+	{
+		case 'A':printf("65");
+				break;
+		case 'B':printf("66");
+				break;
+		case '1':printf("48");
+		         break;
+		/*case 'C':printf("67");
+				break;
+		case 'D':printf("68");
+				break;
+		case 'E':printf("69");
+				break;
+		case 'F':printf("70");
+				break;
+		case 'G':printf("71");
+				break;
+		case 'H':printf("72");
+				break;
+		case 'I':printf("73");
+				break;*/
+
+	}
+}
